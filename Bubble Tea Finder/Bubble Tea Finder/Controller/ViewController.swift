092e7e791b45f80c486e1bc908840c205a69f2ae
Neil Hiddink <nhiddink2@gmail.com>
2018-06-12 14:42:15 -0400

    feat: (Bubble Tea Finder) Implement Practice Batch Request

--- Bubble Tea Finder/Bubble Tea Finder/Controller/ViewController.swift
+++ Bubble Tea Finder/Bubble Tea Finder/Controller/ViewController.swift
@@ -53,6 +53,22 @@ class ViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
     
+    let batchUpdate = NSBatchUpdateRequest(entityName: "Venue")
+    batchUpdate.propertiesToUpdate =
+      [#keyPath(Venue.favorite) : true]
+    batchUpdate.affectedStores =
+      coreDataStack.managedContext
+        .persistentStoreCoordinator?.persistentStores
+    batchUpdate.resultType = .updatedObjectsCountResultType
+    do {
+      let batchResult =
+        try coreDataStack.managedContext.execute(batchUpdate)
+          as! NSBatchUpdateResult
+      print("Records updated \(batchResult.result!)")
+    } catch let error as NSError {
+      print("Could not update \(error), \(error.userInfo)")
+    }
+    
     let venueFetchRequest: NSFetchRequest<Venue> = Venue.fetchRequest()
     fetchRequest = venueFetchRequest
     
