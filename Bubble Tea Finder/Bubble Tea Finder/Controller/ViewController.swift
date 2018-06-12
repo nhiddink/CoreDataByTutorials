3a82c71b1e22e954be7ee0e3abef6b527d95b377
Neil Hiddink <nhiddink2@gmail.com>
2018-06-12 12:54:15 -0400

    feat: (Bubble Tea Finder) Implement Stored Fetch Request for Venue

--- Bubble Tea Finder/Bubble Tea Finder/Controller/ViewController.swift
+++ Bubble Tea Finder/Bubble Tea Finder/Controller/ViewController.swift
@@ -33,46 +33,68 @@ import CoreData
 
 class ViewController: UIViewController {
 
-  // MARK: - Properties
+  // MARK: Properties
   private let filterViewControllerSegueIdentifier = "toFilterViewController"
   fileprivate let venueCellIdentifier = "VenueCell"
 
   var coreDataStack: CoreDataStack!
-
-  // MARK: - IBOutlets
+  var fetchRequest: NSFetchRequest<Venue>?
+  var venues: [Venue] = []
+  
+  // MARK: IB Outlets
   @IBOutlet weak var tableView: UITableView!
 
-  // MARK: - View Life Cycle
+  // MARK: Life Cycle
   override func viewDidLoad() {
     super.viewDidLoad()
+    
+    guard let model = coreDataStack.managedContext.persistentStoreCoordinator?.managedObjectModel,
+          let fetchRequest = model.fetchRequestTemplate(forName: "FetchRequest") as? NSFetchRequest<Venue> else { return }
+    self.fetchRequest = fetchRequest
+    fetchAndReload()
   }
 
-  // MARK: - Navigation
+  // MARK: Navigation
   override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
     if segue.identifier == filterViewControllerSegueIdentifier {
       
     }
   }
-}
-
-// MARK: - IBActions
-extension ViewController {
-
+  
+  // MARK: Helper Methods
+  
+  func fetchAndReload() {
+    
+    guard let fetchRequest = fetchRequest else { return }
+    do {
+      venues = try coreDataStack.managedContext.fetch(fetchRequest)
+      tableView.reloadData()
+    } catch let error as NSError {
+      print("Could not fetch \(error): \(error.userInfo)")
+    }
+    
+  }
+  
+  // MARK: IB Actions
+  
   @IBAction func unwindToVenueListViewController(_ segue: UIStoryboardSegue) {
   }
 }
 
-// MARK: - UITableViewDataSource
+// MARK: UITableView Data Source
+
 extension ViewController: UITableViewDataSource {
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return 10
+    return venues.count
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: venueCellIdentifier, for: indexPath)
-    cell.textLabel?.text = "Bubble Tea Venue"
-    cell.detailTextLabel?.text = "Price Info"
+    let venue = venues[indexPath.row]
+    
+    cell.textLabel?.text = venue.name
+    cell.detailTextLabel?.text = venue.priceInfo?.priceCategory
     return cell
   }
 }
